9cf928a3a089a92dab569eae7469d1f391eb664d
Jose Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2022-03-24 17:25:47 -0300

    exercise 1

--- src/test/java/com/siqueira76/demo/StreamApiExemplesApplicationTests.java
+++ src/test/java/com/siqueira76/demo/StreamApiExemplesApplicationTests.java
@@ -1,13 +1,45 @@
 package com.siqueira76.demo;
 
+import com.siqueira76.demo.models.Product;
+import com.siqueira76.demo.repos.CustomerRepo;
+import com.siqueira76.demo.repos.OrderRepo;
+import com.siqueira76.demo.repos.ProductRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@DataJpaTest
 class StreamApiExemplesApplicationTests {
 
+	@Autowired
+	private CustomerRepo customerRepo;
+
+	@Autowired
+	private OrderRepo orderRepo;
+
+	@Autowired
+	private ProductRepo productRepo;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Obtain a list of product with category = \"Books\" and price > 100")
+	public void exercise1() {
+		long startTime = System.currentTimeMillis();
+		List<Product> result = productRepo.findAll()
+				.stream()
+				.filter(p -> p.getCategory().equalsIgnoreCase("Books"))
+				.filter(p -> p.getPrice() > 100)
+				.collect(Collectors.toList());
+		long endTime = System.currentTimeMillis();
+
+		log.info(String.format("exercise 1 - execution time: %1$d ms", (endTime - startTime)));
+		result.forEach(p -> log.info(p.toString()));
 	}
 
 }
