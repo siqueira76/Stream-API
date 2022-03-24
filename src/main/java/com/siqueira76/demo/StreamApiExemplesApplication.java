d578d42333b46218dec36296a029ebe5e1e2893b
Jose Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2022-03-24 15:38:26 -0300

    first commit

---
+++ src/main/java/com/siqueira76/demo/StreamApiExemplesApplication.java
@@ -0,0 +1,13 @@
+package com.siqueira76.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiExemplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiExemplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siqueira76/demo/StreamApiExemplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siqueira76.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiExemplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
